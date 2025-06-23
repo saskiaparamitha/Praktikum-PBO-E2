224d2da588e4c9037c9eea5648079de92c9e3fdd
saskiaparamitha <saskiaparamitha05@gmail.com>
2025-06-23 13:06:02 +0700

    Create BangunDatarGenerik.java

---
+++ Praktikum8/Tugas/Soal 3.4/BangunDatarGenerik.java
@@ -0,0 +1,46 @@
+class BangunDatarGenerik<T extends BangunDatar> {
+    private T[] kumpulanBangun;
+    private int jumlahBangun;
+    private int kapasitas;
+    
+    @SuppressWarnings("unchecked")
+    public BangunDatarGenerik(int kapasitas) {
+        this.kapasitas = kapasitas;
+        this.kumpulanBangun = (T[]) new BangunDatar[kapasitas];
+        this.jumlahBangun = 0;
+    }
+    
+    public void tambahBangun(T bangun) {
+        if (jumlahBangun < kapasitas) {
+            kumpulanBangun[jumlahBangun] = bangun;
+            jumlahBangun++;
+            System.out.println("Berhasil menambahkan " + bangun.getNama());
+        } else {
+            System.out.println("Kapasitas penuh!");
+        }
+    }
+    
+    public void tampilkanSemuaBangun() {
+        System.out.println("\n=== DAFTAR BANGUN DATAR ===");
+        for (int i = 0; i < jumlahBangun; i++) {
+            System.out.println((i + 1) + ". ");
+            kumpulanBangun[i].tampilkanInfo();
+        }
+    }
+    
+    public double totalLuas() {
+        double total = 0;
+        for (int i = 0; i < jumlahBangun; i++) {
+            total += kumpulanBangun[i].hitungLuas();
+        }
+        return total;
+    }
+    
+    public double totalKeliling() {
+        double total = 0;
+        for (int i = 0; i < jumlahBangun; i++) {
+            total += kumpulanBangun[i].hitungKeliling();
+        }
+        return total;
+    }
+}
